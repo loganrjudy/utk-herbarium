cc46ee9f5a5506c75d8c4fc5fc4832c255b34322
loganrjudy <loganrjudy@gmail.com>
2024-04-25 15:10:13 -0400

    Updated hostname in Connections file

--- public_html/Connections/herbarium.php
+++ public_html/Connections/herbarium.php
@@ -2,7 +2,7 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_Herbarium = "localhost";
+$hostname_Herbarium = "127.0.0.1";
 $database_Herbarium = "herbariumweb_2024";
 $username_Herbarium = "herbariumweb";
 $password_Herbarium = "C0ll3ct1on$2022!";
