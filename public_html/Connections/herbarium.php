62a0c0012fd640b9c2e935749a997d5560478f6c
loganrjudy <loganrjudy@gmail.com>
2024-04-25 14:15:52 -0400

    Removing new from sqli_connect function in Connections file

--- public_html/Connections/herbarium.php
+++ public_html/Connections/herbarium.php
@@ -6,6 +6,5 @@ $hostname_Herbarium = "localhost";
 $database_Herbarium = "herbariumweb_2024";
 $username_Herbarium = "herbariumweb";
 $password_Herbarium = "C0ll3ct1on$2022!";
-$Herbarium = new mysqli_connect($hostname_Herbarium, $username_Herbarium, $password_Herbarium, $database_Herbarium); 
-$result = mysqli_query($Herbarium, $sqlQuery);
+$Herbarium = mysqli_connect($hostname_Herbarium, $username_Herbarium, $password_Herbarium, $database_Herbarium); 
 ?>
