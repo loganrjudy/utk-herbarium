e24e7bf3d93163f475ceea90d97e4cc02405a4a7
loganrjudy <loganrjudy@gmail.com>
2024-04-25 11:49:46 -0400

    Fixing the connections file per John Richmond's instructions

--- public_html/Connections/herbarium.php
+++ public_html/Connections/herbarium.php
@@ -6,5 +6,6 @@ $hostname_Herbarium = "localhost";
 $database_Herbarium = "herbariumweb_2024";
 $username_Herbarium = "herbariumweb";
 $password_Herbarium = "C0ll3ct1on$2022!";
-$Herbarium = new mysqli($hostname_Herbarium, $username_Herbarium, $password_Herbarium, $database_Herbarium) or trigger_error(mysql_error(),E_USER_ERROR); 
+$Herbarium = new mysqli_connect($hostname_Herbarium, $username_Herbarium, $password_Herbarium, $database_Herbarium); 
+$result = mysqli_query($Herbarium, $sqlQuery);
 ?>
